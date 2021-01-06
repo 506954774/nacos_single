7abe5f3fb04e108d34f8d7faaebf3fba0cfe9a27
shun.zhang@ilinklink.com <shun.zhang@ilinklink.com>
2021-01-06 13:16:04 +0800

    使用传统的RestTemplate配合nacos实现rpc调用

--- user-provider/src/main/java/com/example/userprovider/UserProvider.java
+++ user-provider/src/main/java/com/example/userprovider/UserProvider.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,14 +26,14 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-@RequestMapping("/user")
 public class UserProvider {
 
     @Value(value = "${server.port}")
     private int port;
 
     @ApiOperation(value = "测试swagger", notes = "测试swagger")
-    @GetMapping("/test")
+    //@GetMapping("/test")
+    @RequestMapping(value  ="/user/test",method = RequestMethod.GET)
     public ResponseEntity sendByForgetPwd(@ApiParam(name = "message", value = "客户端传来的数据", required = true)
                                           @RequestParam(value = "message") String message) {
 
